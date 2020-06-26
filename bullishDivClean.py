37efd69444e4198d0d48776e3629cba1cc9e96bb
bfan <b.fan@marathon-targets.com>
2020-06-26 22:48:09 +1000

    - Formatting: cleaned up some bad style

--- bullishDivClean.py
+++ bullishDivClean.py
@@ -15,26 +15,21 @@ def buildSigValleyList(valleyList):
     length = len(valleyList)
     
     #add the first valley
-    count = 0
-    for valley in valleyList: 
+    for i,valley in enumerate(valleyList): 
         # first valley is always significant
-        if count == 0:
+        if i == 0:
             sigValley.append(valley)
         else:
             #if the valley right valley and prev valley are > then sig valley 
-            if count < (length-1):
-                left = valleyList[count - 1]
-                right = valleyList[count + 1]
+            if i < (length-1):
+                left = valleyList[i - 1]
+                right = valleyList[i + 1]
                 if (left.rsi > valley.rsi) and (right.rsi > valley.rsi):
                     sigValley.append(valley)
-                    utctime = int(valley.time)/1000
+                    utctime = valley.time//1000
                     pendulum.from_timestamp(utctime).to_datetime_string()
                     newtime = pendulum.from_timestamp(utctime, 'Australia/Sydney').to_datetime_string()
-                            
                     printText = "significant valley @" + str(valley.rsi) + " time: " + str(newtime)
-                    #print(printText)
-           
-        count += 1
 
     return sigValley
 
@@ -73,22 +68,18 @@ while(1):
         for symbol in symbolList:
             url = root_url + '?symbol=' + symbol + '&interval=' + interval
             data = grabData(url)
-            index = 0
             candleAuxList = []
             valleyList = []
 
-            for candle in data: 
+            for index,candle in enumerate(data): 
                 openP = candle[OPEN]
                 closeP = candle[CLOSE]
                 if (index > RSIEP):
                     rsi = crypt.calcRsi(index,data,RSIEP)
                     tempCandleAux = CandleAux(float(rsi), float(closeP), float(candle[OPENTIME]))
                     candleAuxList.append(tempCandleAux)
-                index += 1
 
-            index = 0
-            
-            for auxCandle in candleAuxList:
+            for index,auxCandle in enumerate(candleAuxList):
                 if((index >= 1) and (len(candleAuxList) > 3) and (index < (len(candleAuxList)-1))):
                     prevAux = candleAuxList[index-1]
                     curAux = candleAuxList[index]
@@ -96,31 +87,28 @@ while(1):
                     if((prevAux.rsi > curAux.rsi) and (nextAux.rsi > curAux.rsi)):
                             valleyList.append(curAux)
 
-                index += 1
-
             reversedValleys = valleyList[::-1]
-            index = 0
             divFound = False
            
             reversedValleys = buildSigValleyList(reversedValleys)
 
-            for auxCandle in reversedValleys:
-                if((index) < (len(reversedValleys) - 1)):
+            for index,auxCandle in enumerate(reversedValleys):
+                if index < (len(reversedValleys) - 1):
                     count = 0; 
-                    while(((index + count) < (len(reversedValleys)-1)) and (count < VALLEYS_TO_CHECK)): 
+                    while (((index + count) < (len(reversedValleys)-1)) and (count < VALLEYS_TO_CHECK)): 
                         nextCandle = reversedValleys[index + count]
                         ncPrice = nextCandle.price
                         ncRsi = nextCandle.rsi
                         curPrice = auxCandle.price
                         curRsi = auxCandle.rsi
-                        if ((curRsi > ncRsi) and (curPrice <= ncPrice)):
-                            utctime = int(auxCandle.time)/1000
+                        if (curRsi > ncRsi) and (curPrice <= ncPrice):
+                            utctime = auxCandle.time//1000
                             pendulum.from_timestamp(utctime).to_datetime_string()
                             newtime = pendulum.from_timestamp(utctime, 'Australia/Sydney').to_datetime_string()
                             count = VALLEYS_TO_CHECK
                             tempString = "bullish div@ rsi: " + str(auxCandle.rsi) + " price: " + str(auxCandle.price) + " time: " + newtime + " interval: " + interval
                             key = symbol + interval
-                            if(divDict[key] != tempString):
+                            if divDict[key] != tempString:
                                 print(key)
                                 print(tempString)
                                 divDict[key] = tempString
@@ -128,7 +116,6 @@ while(1):
                             divFound = True
                             break
                         count += 1
-                index += 1
                 if (divFound):
                     break
     time.sleep(25)
--- notify.py
+++ notify.py
@@ -1,8 +1,9 @@
-from slackclient import SlackClient
+from slack import WebClient
+
 def sendMessage(message):
-    token = "ixoxb-517333598005-586257567925-dpppckSdlQBUb9MWFLx83FjU"      # found at https://api.slack.com/web#authentication
-    sc = SlackClient(token)
-    sc.api_call(
-            "chat.postMessage", channel="#general", text=message,
+    token = ""      # found at https://api.slack.com/web#authentication
+    sc = WebClient(token)
+    sc.chat_postMessage(
+            channel="#general", text=message,
             username='sherlock', icon_emoji=':robot_face:'
     )
